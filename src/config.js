78f3acb0c7314975baea316a3c610c83c62ced1c
infomagnum159 <infomagnum159@gmail.com>
2022-07-04 16:57:30 +0600

    Added URLS to config.js

---
+++ src/config.js
@@ -0,0 +1,3 @@
+export const BASE_URL = 'https://restcountries.com/v2/all';
+export const BORDER_URL = 'http://146.185.154.90:8080/restcountries/rest/v2/alpha/';
+export const CHAT_URL = 'http://146.185.154.90:8000/messages';
